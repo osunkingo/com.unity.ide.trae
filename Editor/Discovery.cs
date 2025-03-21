2db423dc82871c4cfade1d91111668b820980c56
bin.sun <bin.sun@fh.com>
2025-03-21 14:07:05 +0800

    [func] find the trae and use it.

--- Editor/Discovery.cs
+++ Editor/Discovery.cs
@@ -13,15 +13,9 @@ namespace Microsoft.Unity.VisualStudio.Editor
 	{
 		public static IEnumerable<IVisualStudioInstallation> GetVisualStudioInstallations()
 		{
-#if UNITY_EDITOR_WIN
-			foreach (var installation in VisualStudioForWindowsInstallation.GetVisualStudioInstallations())
+			foreach (var installation in VisualStudioTraeInstallation.GetVisualStudioInstallations())
 				yield return installation;
-#elif UNITY_EDITOR_OSX
-			foreach (var installation in VisualStudioForMacInstallation.GetVisualStudioInstallations())
-				yield return installation;
-#endif
-
-			foreach (var installation in VisualStudioCodeInstallation.GetVisualStudioInstallations())
+			foreach (var installation in VisualStudioCodiumInstallation.GetVisualStudioInstallations())
 				yield return installation;
 		}
 
@@ -29,14 +23,9 @@ namespace Microsoft.Unity.VisualStudio.Editor
 		{
 			try
 			{
-#if UNITY_EDITOR_WIN
-				if (VisualStudioForWindowsInstallation.TryDiscoverInstallation(editorPath, out installation))
+				if (VisualStudioTraeInstallation.TryDiscoverInstallation(editorPath, out installation))
 					return true;
-#elif UNITY_EDITOR_OSX
-				if (VisualStudioForMacInstallation.TryDiscoverInstallation(editorPath, out installation))
-					return true;
-#endif
-				if (VisualStudioCodeInstallation.TryDiscoverInstallation(editorPath, out installation))
+				if (VisualStudioCodiumInstallation.TryDiscoverInstallation(editorPath, out installation))
 					return true;
 			}
 			catch (IOException)
@@ -49,12 +38,8 @@ namespace Microsoft.Unity.VisualStudio.Editor
 
 		public static void Initialize()
 		{
-#if UNITY_EDITOR_WIN
-			VisualStudioForWindowsInstallation.Initialize();
-#elif UNITY_EDITOR_OSX
-			VisualStudioForMacInstallation.Initialize();
-#endif
-			VisualStudioCodeInstallation.Initialize();
+            VisualStudioTraeInstallation.Initialize();
+            VisualStudioCodiumInstallation.Initialize();
 		}
 	}
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# How to install the plugin for unity
+<br>
+- Search Unity->Window->Package Manager<br>
+- Click "+" left corner<br>
+- Add package from git URL<br>
+- Insert <code>https://github.com/osunkingo/com.unity.ide.trae.git</code><br>
+- Add<br>
+- Wait for a few seconds, Done.
